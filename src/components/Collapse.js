c60573906b631f5b7544b273d7e489dbf41b7531
Gagnaire Pauline <gagnaire.pauline@gmail.com>
2023-06-19 09:27:03 +0200

    Lissage de l'animation de la collapse

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
 import "../styles/components/_collapse.scss";
 
 const Collapse = ({ title, description }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const descriptionRef = useRef(null);
 
   const toggleCollapse = () => {
     setIsOpen(!isOpen);
+  };
+
+  const getDescriptionHeight = () => {
+    return isOpen ? descriptionRef.current.scrollHeight : 0;
   };
 
   return (
@@ -23,9 +28,17 @@ const Collapse = ({ title, description }) => {
             className={`rotate-icon ${isOpen ? "rotate-icon-open" : ""}`}
           />
         </p>
-        <div className="description" style={{
-          maxHeight: isOpen ? 300 : 0
-        }}>{description}</div>
+        <div
+          className="description"
+          style={{
+            maxHeight: getDescriptionHeight(),
+            overflow: "hidden",
+            transition: "max-height 0.3s ease-in-out",
+          }}
+          ref={descriptionRef}
+        >
+          {description}
+        </div>
       </div>
     </li>
   );
